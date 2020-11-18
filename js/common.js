bc42bdbf36f8207a4a3a49dfde75199564567b43
yujeongcmyk <73566200+yujeongcmyk@users.noreply.github.com>
2020-11-18 16:35:13 +0900

    hash scroll animate update
    애니메이션 부드럽게 이동

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 $(function(){
- alert("test");
+ // $("nav ul li").each(function(){
+ //   var num = $(this).find("a");
+ //   console.log( $(num.hash))
+ // });
+$("nav ul li a, .back_to_top a, .footer_top a, a.button").click(function(){
+  // console.log($(this.hash).offset().top)//hash코드를 가져오겠다.
+  var thisElem = $(this.hash);
+  $("html,body").stop();
+  $("html,body").animate({scrollTop : thisElem.offset().top},1500);//1.5초
+
+  //e.preventDefault();
+  return false;
+ });
 });//document ready
